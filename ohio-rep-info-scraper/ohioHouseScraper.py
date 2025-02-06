a823e61d7c97f9f37b672568d8a4f36019a41c72
khowellmobile <khowellmobile@gmail.com>
2025-02-06 14:15:05 -0500

    Added multithreading

--- ohio-rep-info-scraper/ohioHouseScraper.py
+++ ohio-rep-info-scraper/ohioHouseScraper.py
@@ -1,4 +1,6 @@
 import requests  # type: ignore
+import threading
+import queue
 import time
 from bs4 import BeautifulSoup  # type: ignore
 import google.generativeai as genai  # type: ignore
@@ -41,8 +43,9 @@ ai_prompt_text = """
     Biography:
     """
 
+
 def main():
-    """ rep_names = ["munira-abdullahi", "darnell-t-brewer", "karen-brownlee"] """
+    """rep_names = ["munira-abdullahi", "darnell-t-brewer", "karen-brownlee"]"""
     rep_names = []
     url = "https://ohiohouse.gov/members/directory?start=1&sort=LastName"
     response = requests.get(url)
@@ -51,75 +54,40 @@ def main():
 
     rep_name_divs = soup.find_all("div", class_="media-overlay-caption-text-line-1")
 
-    for div in rep_name_divs[:10]:
+    for div in rep_name_divs[:30]:
         # Use re.sub in future
         rep_names.append(
             div.text.strip().replace(" ", "-").replace(".", "").replace(",", "").lower()
         )
 
+    people_queue, error_queue = batch_processor(rep_names)
+
     people = {}
+    while not people_queue.empty():
+        people.update(people_queue.get())
 
-    # Throttled loop to ensure it does not exceed rate limit for AI Calls
-    for rep_name in rep_names:
-        start_time = time.time()
-
-        people[rep_name] = {}
-        getInfo(people, rep_name)
-
-        elapsed_time = time.time() - start_time 
-        start_time = time.time()
-        print(elapsed_time, "getInfo")
-
-        fillBioFields(people, rep_name, getBio(rep_name))
-
-        elapsed_time = time.time() - start_time 
-        start_time = time.time()
-        print(elapsed_time, "Bio fields")
-
-        getCommittees(people, rep_name)
-
-        elapsed_time = time.time() - start_time 
-        print(elapsed_time, "Comittees")
-        """ time_to_wait = 4 - elapsed_time 
-
-        if time_to_wait > 0:
-            time.sleep(time_to_wait)  """
-        
-        print("_-----------------------------------------------+_")
-
-    print(people)
+    
 
 
-def checkResponse(response):
-    if response.status_code:
-        if response.status_code != 200:
-            print("Bad response code: ", response.status_code)
-            return 1
-        else:
-            return 0
-    else:
-        return 0
-
-
-def getInfo(people, rep_name):
+def getInfo(rep_name):
     address_keywords = ["77", "High", "Street", "St.", "South", "S.", "Floor"]
 
     url = f"https://ohiohouse.gov/members/{rep_name}"
     response = requests.get(url)
 
     if checkResponse(response) != 0:
-        print(rep_name, "Bad Request")
-        print("-------------------------")
-        return
+        return None
 
     soup = BeautifulSoup(response.content, "html.parser")
 
     divs = soup.find_all("div", class_="member-info-bar-module")
 
+    home_town = address = phone_number = fax_number = "Not Listed"
+
     for module in divs:
         module_text = module.get_text()
         if "Hometown" in module_text:
-            people[rep_name]["home_town"] = module.find(
+            home_town = module.find(
                 "div", class_="member-info-bar-value"
             ).text.strip()
 
@@ -128,13 +96,15 @@ def getInfo(people, rep_name):
                 "div", class_="member-info-bar-value"
             )
 
-            people[rep_name]["office_address"] = address_number_module[0].text.strip()
-            people[rep_name]["phone_number"] = (
+            address = address_number_module[0].text.strip()
+            phone_number = (
                 address_number_module[1].text.strip().replace("Phone: ", "")
             )  # remove preceding "Phone: "
-            people[rep_name]["fax_number"] = (
+            fax_number = (
                 address_number_module[2].text.strip().replace("Fax: ", "")
             )  # remove preceding "Fax: "
+
+    return home_town, address, phone_number, fax_number
 
 
 def getBio(rep_name):
@@ -143,9 +113,7 @@ def getBio(rep_name):
     response = requests.get(url)
 
     if checkResponse(response) != 0:
-        print(rep_name, "Bad Request")
-        print("-------------------------")
-        return
+        return None
 
     soup = BeautifulSoup(response.content, "html.parser")
 
@@ -159,12 +127,22 @@ def getBio(rep_name):
                 paragraph.text.strip() for paragraph in bio_paragraphs
             )
 
-            return combined_bio
+            response = model.generate_content(ai_prompt_text + " " + combined_bio)
+
+            values = response.text.split("|")
+
+            if len(values) < 4:
+                return "AI Error"
+
+            return values[0], values[1], values[2], values[3]
 
 
-def getCommittees(people, rep_name):
+def getCommittees(rep_name):
     url = f"https://ohiohouse.gov/members/{rep_name}/committees"
     response = requests.get(url)
+
+    if checkResponse(response) != 0:
+        return None
 
     soup = BeautifulSoup(response.content, "html.parser")
 
@@ -172,19 +150,78 @@ def getCommittees(people, rep_name):
 
     committees = ",".join(caption.text.strip() for caption in media_captions)
 
-    people[rep_name]["committees"] = committees
+    return committees
 
-def fillBioFields(people, rep_name, bio):
+def checkResponse(response):
+    if response.status_code:
+        if response.status_code != 200:
+            print("Bad response code: ", response.status_code)
+            return 1
+        else:
+            return 0
+    else:
+        return 0
 
-    response = model.generate_content(ai_prompt_text + " " + bio)
 
-    values = response.text.split("|")
-    
-    people[rep_name]["education"] = values[0]
-    people[rep_name]["political"] = values[1]
-    people[rep_name]["employment"] = values[2]
-    people[rep_name]["community"] = values[3] 
+# Process one rep and returns a person object
+def process_rep(rep_name, result_queue, error_queue):
+    rep_obj = {}
 
+    def fetch_function_results(func, func_name, rep_name):
+        rep_obj[func_name] = func(rep_name)
+
+    threads = [
+        threading.Thread(target=fetch_function_results, args=(getInfo, "getInfo", rep_name)),
+        threading.Thread(target=fetch_function_results, args=(getBio, "getBio", rep_name)),
+        threading.Thread(target=fetch_function_results, args=(getCommittees, "getCommittees", rep_name))
+    ]
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads: 
+        thread.join()
+
+    if rep_obj['getBio'] == "AI Error":
+        error_queue.put(rep_name)
+
+    result_queue.put({rep_name: rep_obj})
+
+
+def process_batch(batch, result_queue, error_queue):
+    batch_threads = []
+
+    for rep_name in batch:
+        batch_thread = threading.Thread(target=process_rep, args=(rep_name, result_queue, error_queue))
+        batch_threads.append(batch_thread)
+        batch_thread.start()
+        time.sleep(3)
+        
+    for batch_thread in batch_threads:
+        batch_thread.join()
+
+
+def batch_processor(inputs, batch_size=15, total_batches=2, interval=60):
+    batches = [inputs[i : i + batch_size] for i in range(0, len(inputs), batch_size)]
+    result_queue = queue.Queue()
+    error_queue = queue.Queue()
+    batch_threads = []
+
+    for i in range(total_batches):
+        if i < len(batches):
+            batch = batches[i]
+            print(f"Starting batch {i + 1}/{total_batches}...")
+
+            batch_thread = threading.Thread(target=process_batch, args=(batch, result_queue, error_queue))
+            batch_thread.start()
+
+            print(f"Waiting for {interval} seconds before starting the next batch...")
+            time.sleep(interval)
+
+    for batch_thread in batch_threads:
+        batch_thread.join()
+
+    return result_queue, error_queue
 
 
 if __name__ == "__main__":
